d09dd344f767a785f8b0d0bdb9e2b0f0749f04fc
margaritakovalchuk <margoshakovalchyk@gmail.com>
2019-09-08 11:49:29 +0300

    Implemented sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,24 @@
 var gulp = require('gulp'),
-    gulpSass = require('gulp-sass');
+    gulpSass = require('gulp-sass'),
+    browserSync = require('browser-sync').create();
 
-function sass () {
+function sass() {
     return gulp
         .src('./src/sass/**/*.scss')
         .pipe(gulpSass().on('error', gulpSass.logError))
-        .pipe(gulp.dest('./dist/css'));
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(browserSync.stream());
 }
 
-function watch () {
-    gulp.watch('./src/sass/**/*', gulp.series(sass));
+function sync() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch('src/sass/**/*', gulp.series(sass));
+    gulp.watch("*.html").on('change', browserSync.reload);
 }
 
 exports.sass = gulp.series(sass);
-exports.build = gulp.series(watch);
+exports.build = gulp.series(sync);

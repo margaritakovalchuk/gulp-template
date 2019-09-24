058f30ef864315cccf8a81d1a434f6c3733ef2e8
margaritakovalchuk <margoshakovalchyk@gmail.com>
2019-09-24 11:14:26 +0300

    Implemented cleanCSS and autoPrefix

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 var gulp = require('gulp'),
     gulpSass = require('gulp-sass'),
-    browserSync = require('browser-sync').create();
+    browserSync = require('browser-sync').create(),
+    autoPrefix = require('gulp-autoprefixer'),
+    cleanCSS = require('gulp-clean-css'),
+    sourcemaps  = require('gulp-sourcemaps');
 
 function sass() {
     return gulp
         .src('./src/sass/**/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(gulpSass().on('error', gulpSass.logError))
+        .pipe(cleanCSS())
+        .pipe(autoPrefix())
         .pipe(gulp.dest('./dist/css'))
         .pipe(browserSync.stream());
 }
